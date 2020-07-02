f6c21d4e8ffebb45052fb082a9ca05662047a4ac
Alina Chernavskikh <alina.chernavskikh@eltexsoft.com>
2020-07-02 19:10:02 +0300

    Add get posts query

--- resolvers.js
+++ resolvers.js
@@ -2,7 +2,15 @@
 
 module.exports = {
     Query: {
-        getUser: () => null
+        getUser: () => null,
+        getPosts: async (_, args, { Post }) => {
+            const posts = await Post.find({}).sort({ createdDate: 'desc' }).populate({
+                path: 'createdBy',
+                model: 'User',
+            });
+
+            return posts;
+        }
     },
     Mutation: {
         // { Post } => Post model
