ceaead458a97fb0f5421f82c8ce830e3f059b42b
Alina Chernavskikh <alina@eltexsoft.com>
2020-10-26 23:38:01 +0200

    Add few standart gql requests and client side with vuetify

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <h1>App</h1>
+    <router-view/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-left">Home</h1>
+    <v-btn color="primary">Button</v-btn>
+    <v-btn color="error">Button</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
