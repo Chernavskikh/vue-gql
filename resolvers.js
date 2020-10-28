17b544ae8cdd2f7c1449248472848a229c499f8a
Alina Chernavskikh <alina@eltexsoft.com>
2020-10-28 23:54:59 +0200

    Add signin mutation

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,5 @@
 // A map of functions which return data for the schema.
+const bcrypt = require('bcrypt');
 
 module.exports = {
     Query: {
@@ -24,6 +25,18 @@ module.exports = {
             }).save();
 
             return newPost;
+        },
+        signinUser: async (_, { username, password }, { User }) => {
+            const user = await User.findOne({ username });
+            if (!user) {
+                throw new Error('User not found');
+            }
+            const isValidPassword = await bcrypt.compare(password, user.password);
+            if (!isValidPassword) {
+                throw new Error('Invalid Password');
+            }
+
+            return user;
         },
         signupUser: async (_, { username, email, password }, { User }, info) => {
             const user = await User.findOne({ username: username });
